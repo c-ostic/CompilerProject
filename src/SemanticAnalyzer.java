12cdf61a62939b5c61d9bca9906f47621be0606d
c-ostic <60513574+c-ostic@users.noreply.github.com>
2022-04-02 15:33:10 -0400

    AST Recursive Descent Part 1
    Set up the beginnings of a modified recursive descent through the CST to create the AST
    Theoretically this should work
    In practice? We'll see

--- src/SemanticAnalyzer.java
+++ src/SemanticAnalyzer.java
@@ -32,6 +32,49 @@ public class SemanticAnalyzer
         //reset all the necessary values
         reset();
 
+        createAST(cst.getRoot());
+
         return ast;
+    }
+
+    // Modified recursive descent through CST to create AST
+
+    private void createAST(SyntaxTreeNode cstRoot)
+    {
+        //every CST must start with a Program node
+        ast.addRootNode("Program");
+        //the first child of a Program node is a Block
+        createBlock(cstRoot.getChildren().get(0));
+        ast.moveUp();
+    }
+
+    private void createBlock(SyntaxTreeNode current)
+    {
+        ast.addBranchNode("Block");
+    }
+
+    private void createPrintStatement(SyntaxTreeNode current)
+    {
+
+    }
+
+    private void createAssignStatement(SyntaxTreeNode current)
+    {
+
+    }
+
+    private void createVarDeclStatement(SyntaxTreeNode current)
+    {
+
+    }
+
+    private void createWhileStatement(SyntaxTreeNode current)
+    {
+
+    }
+
+    private void createIfStatement(SyntaxTreeNode current)
+    {
+
     }
 }
