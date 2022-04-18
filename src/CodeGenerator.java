a6f546dc07edaef0c8191fcc699d2aaf1aa9b670
c-ostic <60513574+c-ostic@users.noreply.github.com>
2022-04-18 12:51:21 -0400

    Code Gen Class
    Create base class for code generator

---
+++ src/CodeGenerator.java
@@ -0,0 +1,41 @@
+public class CodeGenerator
+{
+    //set the executable size
+    private final int EXE_SIZE = 0x100;
+
+    private String[] executable;
+    private int heapStart;
+    private int programNum;
+
+    public CodeGenerator()
+    {
+        reset();
+    }
+
+    public void reset()
+    {
+        //create the executable and default all to 00
+        executable = new String[EXE_SIZE];
+        for(int i = 0;i < executable.length;i++)
+            executable[i] = "00";
+
+        //set the start of the heap to the end of the executable
+        heapStart = EXE_SIZE - 1;
+    }
+
+    public void tryCodeGeneration(SyntaxTree ast, int program, boolean hadPrevError)
+    {
+        //reset all the necessary values
+        reset();
+
+        //save the program number
+        programNum = program;
+
+        //before doing anything, if lex, parse, or semantic analysis had an error, skip code generation
+        if(hadPrevError)
+        {
+            System.out.println("Code Generation for Program " + program + " skipped due to previous errors");
+            return;
+        }
+    }
+}
