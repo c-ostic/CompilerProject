6ab91fbd0be0ca75eb64436d15e0bbad3d26fe3d
c-ostic <60513574+c-ostic@users.noreply.github.com>
2022-04-18 14:46:20 -0400

    Code Gen method skeleton
    Skeleton for descending through the ast to generate code

--- src/CodeGenerator.java
+++ src/CodeGenerator.java
@@ -41,5 +41,96 @@ public class CodeGenerator
             System.out.println("Code Generation for Program " + program + " skipped due to previous errors");
             return;
         }
+
+        generateProgram(ast);
+    }
+
+    /*-------------------------------------------- Code Gen Methods --------------------------------------------------*/
+
+    private void generateProgram(SyntaxTree ast)
+    {
+        //the first child of the root is the first block in the program
+        //get the code in the form of a space delineated string
+        String codeString = generateBlock(ast.getRoot().getChild(0));
+
+        //iterate through the full string to put it into the executable
+
+        //backpatch the table, then iterate through the array changing necessary parts
+    }
+
+    private String generateBlock(SyntaxTreeNode blockNode)
+    {
+        //generates the code in string form for easier concatenation of program lines
+        String codeString = "";
+
+        for(SyntaxTreeNode child : blockNode.getChildren())
+        {
+            switch (child.getNodeType())
+            {
+                case PRINT_STATEMENT:
+                {
+                    codeString += generatePrint(child);
+                    break;
+                }
+                case ASSIGNMENT_STATEMENT:
+                {
+                    codeString += generateAssignment(child);
+                    break;
+                }
+                case VAR_DECL:
+                {
+                    codeString += generateVarDecl(child);
+                    break;
+                }
+                case WHILE_STATEMENT:
+                {
+                    codeString += generateWhile(child);
+                    break;
+                }
+                case IF_STATEMENT:
+                {
+                    codeString += generateIf(child);
+                    break;
+                }
+                case BLOCK:
+                {
+                    //recursively call the next block
+                    codeString += generateBlock(child);
+                    break;
+                }
+            }
+        }
+
+        return codeString;
+    }
+
+    private String generatePrint(SyntaxTreeNode printNode)
+    {
+        String codeString = "";
+        return codeString;
+    }
+
+    private String generateAssignment(SyntaxTreeNode assignmentNode)
+    {
+        String codeString = "";
+        return codeString;
+    }
+
+    private String generateVarDecl(SyntaxTreeNode varDeclNode)
+    {
+        String codeString = "";
+        return codeString;
+    }
+
+    private String generateWhile(SyntaxTreeNode whileNode)
+    {
+        String codeString = "";
+        return codeString;
+    }
+
+    private String generateIf(SyntaxTreeNode ifNode)
+    {
+        String codeString = "";
+        return codeString;
     }
 }
